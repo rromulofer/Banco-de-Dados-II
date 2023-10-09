b0484eccedc2aa6ef99f6af2c36d76fd00fce731
Rômulo Souza Fernandes <00119110559@pq.uenf.br>
2023-10-09 14:23:43 -0300

    update

--- Projeto-2/Aplicacao-Concessionaria.py
+++ Projeto-2/Aplicacao-Concessionaria.py
@@ -2,10 +2,14 @@ import sqlite3
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import Scrollbar
+from tkinter import *
+from tkinter import ttk
 
+banco = sqlite3.connect("concessionaria2.db")
+cursor = banco.cursor()
 
 # Função para atualizar moto
-def atualizar_moto():
+def atualizar_moto(cursor, banco):
     try:
         moto_id_para_atualizar = int(id_atualizar_entry.get())
         campo = campo_atualizar_entry.get()
@@ -17,7 +21,7 @@ def atualizar_moto():
         messagebox.showerror("Erro", f"Erro ao atualizar moto: {erro}")
 
 # Função para remover moto
-def remover_moto():
+def remover_moto(cursor, banco):
     try:
         moto_id_para_deletar = int(id_remover_entry.get())
         cursor.execute("DELETE FROM Moto WHERE IDMoto = ?", (moto_id_para_deletar,))
@@ -27,7 +31,7 @@ def remover_moto():
         messagebox.showerror("Erro", f"Erro ao remover moto: {erro}")
 
 # Função para adicionar moto
-def adicionar_moto():
+def adicionar_moto(cursor, banco):
     try:
         modelo = modelo_adicionar_entry.get()
         ano = int(ano_adicionar_entry.get())
@@ -41,21 +45,21 @@ def adicionar_moto():
     except sqlite3.Error as erro:
         messagebox.showerror("Erro", f"Erro ao adicionar moto: {erro}")
 
-# Função para consultar motos
-def consultar_moto():
-    try:
-        cursor.execute("SELECT * FROM Moto")
-        resultados = cursor.fetchall()
-        resultado_text.config(state=tk.NORMAL)
-        resultado_text.delete("1.0", tk.END)
-        for resultado in resultados:
-            resultado_text.insert(tk.END, f"ID: {resultado[0]}, Modelo: {resultado[1]}, Ano: {resultado[2]}, "
-                                          f"Preço: {resultado[3]}, Cor: {resultado[4]}, IDCompra: {resultado[5]}\n")
-        resultado_text.config(state=tk.DISABLED)
-    except sqlite3.Error as erro:
-        messagebox.showerror("Erro", f"Erro ao consultar moto: {erro}")
+# # Função para consultar motos
+# def consultar_moto(cursor, banco):
+#     try:
+#         cursor.execute("SELECT * FROM Moto")
+#         resultados = cursor.fetchall()
+#         resultado_text.config(state=tk.NORMAL)
+#         resultado_text.delete("1.0", tk.END)
+#         for resultado in resultados:
+#             resultado_text.insert(tk.END, f"ID: {resultado[0]}, Modelo: {resultado[1]}, Ano: {resultado[2]}, "
+#                                           f"Preço: {resultado[3]}, Cor: {resultado[4]}, IDCompra: {resultado[5]}\n")
+#         resultado_text.config(state=tk.DISABLED)
+#     except sqlite3.Error as erro:
+#         messagebox.showerror("Erro", f"Erro ao consultar moto: {erro}")
 
-def atualizar_cliente():
+def atualizar_cliente(cursor, banco):
     try:
         cliente_id_para_atualizar = int(id_atualizar_cliente_entry.get())
         campo = campo_atualizar_cliente_entry.get()
@@ -67,7 +71,7 @@ def atualizar_cliente():
         messagebox.showerror("Erro", f"Erro ao atualizar dados do cliente: {erro}")
 
 # Função para remover dados de Cliente
-def remover_cliente():
+def remover_cliente(cursor, banco):
     try:
         cliente_id_para_deletar = int(id_remover_cliente_entry.get())
         cursor.execute("DELETE FROM Clientes WHERE IDCliente = ?", (cliente_id_para_deletar,))
@@ -77,7 +81,7 @@ def remover_cliente():
         messagebox.showerror("Erro", f"Erro ao remover dados do cliente: {erro}")
 
 # Função para adicionar dados de Cliente
-def adicionar_cliente():
+def adicionar_cliente(cursor, banco):
     try:
         nome = nome_adicionar_cliente_entry.get()
         idade = int(idade_adicionar_cliente_entry.get())
@@ -92,7 +96,7 @@ def adicionar_cliente():
         messagebox.showerror("Erro", f"Erro ao adicionar dados do cliente: {erro}")
 
 # Função para atualizar dados de Compra
-def atualizar_compra():
+def atualizar_compra(cursor, banco):
     try:
         compra_id_para_atualizar = int(id_atualizar_compra_entry.get())
         campo = campo_atualizar_compra_entry.get()
@@ -104,7 +108,7 @@ def atualizar_compra():
         messagebox.showerror("Erro", f"Erro ao atualizar dados da compra: {erro}")
 
 # Função para remover dados de Compra
-def remover_compra():
+def remover_compra(cursor, banco):
     try:
         compra_id_para_deletar = int(id_remover_compra_entry.get())
         cursor.execute("DELETE FROM Compra WHERE IDCompra = ?", (compra_id_para_deletar,))
@@ -114,7 +118,7 @@ def remover_compra():
         messagebox.showerror("Erro", f"Erro ao remover dados da compra: {erro}")
 
 # Função para adicionar dados de Compra
-def adicionar_compra():
+def adicionar_compra(cursor, banco):
     try:
         data_compra = data_compra_adicionar_entry.get()
         valor = float(valor_adicionar_compra_entry.get())
@@ -127,57 +131,21 @@ def adicionar_compra():
         messagebox.showerror("Erro", f"Erro ao adicionar dados da compra: {erro}")
 
 
-def mostrar_janela_moto():
-    # Esconder outras janelas
-    cliente_frame.pack_forget()
-    compra_frame.pack_forget()
+# Crie uma instância do Tkinter
+root = tk.Tk()
+root.title("Sistema de Controle de Motos")
 
-    # Mostrar janela de Moto
-    moto_frame.pack()
+# Crie um Notebook para abas
+notebook = ttk.Notebook(root)
+notebook.pack(fill="both", expand=True)
 
-def mostrar_janela_cliente():
-    # Esconder outras janelas
-    moto_frame.pack_forget()
-    compra_frame.pack_forget()
+# Aba para Moto
+frame_moto = ttk.Frame(notebook)
+notebook.add(frame_moto, text="Moto")
 
-    # Mostrar janela de Cliente
-    cliente_frame.pack()
-
-def mostrar_janela_compra():
-    # Esconder outras janelas
-    moto_frame.pack_forget()
-    cliente_frame.pack_forget()
-
-    # Mostrar janela de Compra
-    compra_frame.pack()
-
-
-# Configuração da janela principal
-janela = tk.Tk()
-janela.title("Concessionária")
-
-# Conexão com o banco de dados
-banco = sqlite3.connect('concessionaria2.db')
-cursor = banco.cursor()
-
-
-# Crie os frames para cada opção
-moto_frame = tk.Frame(janela)
-cliente_frame = tk.Frame(janela)
-compra_frame = tk.Frame(janela)
-
-# Crie os botões no menu para cada opção
-menu_frame = tk.Frame(janela)
-menu_frame.pack()
-moto_button = tk.Button(menu_frame, text="Moto", command=mostrar_janela_moto)
-moto_button.grid(row=0, column=0)
-cliente_button = tk.Button(menu_frame, text="Cliente", command=mostrar_janela_cliente)
-cliente_button.grid(row=0, column=1)
-compra_button = tk.Button(menu_frame, text="Compras", command=mostrar_janela_compra)
-compra_button.grid(row=0, column=2)
-
+# Adicione widgets à aba Moto
 # Formulário para atualizar moto
-atualizar_frame = tk.Frame(janela)
+atualizar_frame = tk.Frame(frame_moto)
 atualizar_frame.pack()
 atualizar_label = tk.Label(atualizar_frame, text="Atualizar Moto")
 atualizar_label.grid(row=0, column=0, columnspan=2)
@@ -193,11 +161,11 @@ novo_valor_atualizar_label = tk.Label(atualizar_frame, text="Novo Valor:")
 novo_valor_atualizar_label.grid(row=3, column=0)
 novo_valor_atualizar_entry = tk.Entry(atualizar_frame)
 novo_valor_atualizar_entry.grid(row=3, column=1)
-atualizar_button = tk.Button(atualizar_frame, text="Atualizar", command=atualizar_moto)
+atualizar_button = tk.Button(atualizar_frame, text="Atualizar", command=lambda: atualizar_moto(cursor, banco))
 atualizar_button.grid(row=4, column=0, columnspan=2)
 
 # Formulário para remover moto
-remover_frame = tk.Frame(janela)
+remover_frame = tk.Frame(frame_moto)
 remover_frame.pack()
 remover_label = tk.Label(remover_frame, text="Remover Moto")
 remover_label.grid(row=0, column=0, columnspan=2)
@@ -205,11 +173,11 @@ id_remover_label = tk.Label(remover_frame, text="ID da Moto:")
 id_remover_label.grid(row=1, column=0)
 id_remover_entry = tk.Entry(remover_frame)
 id_remover_entry.grid(row=1, column=1)
-remover_button = tk.Button(remover_frame, text="Remover", command=remover_moto)
+remover_button = tk.Button(remover_frame, text="Remover", command=lambda: remover_moto(cursor, banco))
 remover_button.grid(row=2, column=0, columnspan=2)
 
 # Formulário para adicionar moto
-adicionar_frame = tk.Frame(janela)
+adicionar_frame = tk.Frame(frame_moto)
 adicionar_frame.pack()
 adicionar_label = tk.Label(adicionar_frame, text="Adicionar Moto")
 adicionar_label.grid(row=0, column=0, columnspan=2)
@@ -233,14 +201,17 @@ id_compra_adicionar_label = tk.Label(adicionar_frame, text="ID da Compra (opcion
 id_compra_adicionar_label.grid(row=5, column=0)
 id_compra_adicionar_entry = tk.Entry(adicionar_frame)
 id_compra_adicionar_entry.grid(row=5, column=1)
-adicionar_button = tk.Button(adicionar_frame, text="Adicionar", command=adicionar_moto)
+adicionar_button = tk.Button(adicionar_frame, text="Adicionar", command=lambda: adicionar_moto(cursor, banco))
 adicionar_button.grid(row=6, column=0, columnspan=2)
 
 
 
+# Aba para Cliente (exemplo de outra aba)
+frame_cliente = ttk.Frame(notebook)
+notebook.add(frame_cliente, text="Cliente")
 
 # Formulário para atualizar dados de Cliente
-atualizar_cliente_frame = tk.Frame(janela)
+atualizar_cliente_frame = tk.Frame(frame_cliente)
 atualizar_cliente_frame.pack()
 atualizar_cliente_label = tk.Label(atualizar_cliente_frame, text="Atualizar Dados do Cliente")
 atualizar_cliente_label.grid(row=0, column=0, columnspan=2)
@@ -256,11 +227,11 @@ novo_valor_atualizar_cliente_label = tk.Label(atualizar_cliente_frame, text="Nov
 novo_valor_atualizar_cliente_label.grid(row=3, column=0)
 novo_valor_atualizar_cliente_entry = tk.Entry(atualizar_cliente_frame)
 novo_valor_atualizar_cliente_entry.grid(row=3, column=1)
-atualizar_cliente_button = tk.Button(atualizar_cliente_frame, text="Atualizar", command=atualizar_cliente)
+atualizar_cliente_button = tk.Button(atualizar_cliente_frame, text="Atualizar", command=lambda: atualizar_cliente(cursor, banco))
 atualizar_cliente_button.grid(row=4, column=0, columnspan=2)
 
 # Formulário para remover dados de Cliente
-remover_cliente_frame = tk.Frame(janela)
+remover_cliente_frame = tk.Frame(frame_cliente)
 remover_cliente_frame.pack()
 remover_cliente_label = tk.Label(remover_cliente_frame, text="Remover Dados do Cliente")
 remover_cliente_label.grid(row=0, column=0, columnspan=2)
@@ -268,11 +239,11 @@ id_remover_cliente_label = tk.Label(remover_cliente_frame, text="ID do Cliente:"
 id_remover_cliente_label.grid(row=1, column=0)
 id_remover_cliente_entry = tk.Entry(remover_cliente_frame)
 id_remover_cliente_entry.grid(row=1, column=1)
-remover_cliente_button = tk.Button(remover_cliente_frame, text="Remover", command=remover_cliente)
+remover_cliente_button = tk.Button(remover_cliente_frame, text="Remover", command=lambda: remover_cliente(cursor, banco))
 remover_cliente_button.grid(row=2, column=0, columnspan=2)
 
 # Formulário para adicionar dados de Cliente
-adicionar_cliente_frame = tk.Frame(janela)
+adicionar_cliente_frame = tk.Frame(frame_cliente)
 adicionar_cliente_frame.pack()
 adicionar_cliente_label = tk.Label(adicionar_cliente_frame, text="Adicionar Dados do Cliente")
 adicionar_cliente_label.grid(row=0, column=0, columnspan=2)
@@ -296,14 +267,17 @@ endereco_adicionar_cliente_label = tk.Label(adicionar_cliente_frame, text="Ender
 endereco_adicionar_cliente_label.grid(row=5, column=0)
 endereco_adicionar_cliente_entry = tk.Entry(adicionar_cliente_frame)
 endereco_adicionar_cliente_entry.grid(row=5, column=1)
-adicionar_cliente_button = tk.Button(adicionar_cliente_frame, text="Adicionar", command=adicionar_cliente)
+adicionar_cliente_button = tk.Button(adicionar_cliente_frame, text="Adicionar", command=lambda: adicionar_cliente(cursor, banco))
 adicionar_cliente_button.grid(row=6, column=0, columnspan=2)
 
 
 
+# Aba para Compras (exemplo de outra aba)
+frame_compra = ttk.Frame(notebook)
+notebook.add(frame_compra, text="Compras")
 
 # Formulário para atualizar dados de Compra
-atualizar_compra_frame = tk.Frame(janela)
+atualizar_compra_frame = tk.Frame(frame_compra)
 atualizar_compra_frame.pack()
 atualizar_compra_label = tk.Label(atualizar_compra_frame, text="Atualizar Dados da Compra")
 atualizar_compra_label.grid(row=0, column=0, columnspan=2)
@@ -319,11 +293,11 @@ novo_valor_atualizar_compra_label = tk.Label(atualizar_compra_frame, text="Novo 
 novo_valor_atualizar_compra_label.grid(row=3, column=0)
 novo_valor_atualizar_compra_entry = tk.Entry(atualizar_compra_frame)
 novo_valor_atualizar_compra_entry.grid(row=3, column=1)
-atualizar_compra_button = tk.Button(atualizar_compra_frame, text="Atualizar", command=atualizar_compra)
+atualizar_compra_button = tk.Button(atualizar_compra_frame, text="Atualizar", command=lambda: atualizar_compra(cursor, banco))
 atualizar_compra_button.grid(row=4, column=0, columnspan=2)
 
 # Formulário para remover dados de Compra
-remover_compra_frame = tk.Frame(janela)
+remover_compra_frame = tk.Frame(frame_compra)
 remover_compra_frame.pack()
 remover_compra_label = tk.Label(remover_compra_frame, text="Remover Dados da Compra")
 remover_compra_label.grid(row=0, column=0, columnspan=2)
@@ -331,11 +305,11 @@ id_remover_compra_label = tk.Label(remover_compra_frame, text="ID da Compra:")
 id_remover_compra_label.grid(row=1, column=0)
 id_remover_compra_entry = tk.Entry(remover_compra_frame)
 id_remover_compra_entry.grid(row=1, column=1)
-remover_compra_button = tk.Button(remover_compra_frame, text="Remover", command=remover_compra)
+remover_compra_button = tk.Button(remover_compra_frame, text="Remover", command=lambda: remover_compra(cursor, banco))
 remover_compra_button.grid(row=2, column=0, columnspan=2)
 
 # Formulário para adicionar dados de Compra
-adicionar_compra_frame = tk.Frame(janela)
+adicionar_compra_frame = tk.Frame(frame_compra)
 adicionar_compra_frame.pack()
 adicionar_compra_label = tk.Label(adicionar_compra_frame, text="Adicionar Dados da Compra")
 adicionar_compra_label.grid(row=0, column=0, columnspan=2)
@@ -351,17 +325,11 @@ id_cliente_adicionar_compra_label = tk.Label(adicionar_compra_frame, text="ID do
 id_cliente_adicionar_compra_label.grid(row=3, column=0)
 id_cliente_adicionar_compra_entry = tk.Entry(adicionar_compra_frame)
 id_cliente_adicionar_compra_entry.grid(row=3, column=1)
-adicionar_compra_button = tk.Button(adicionar_compra_frame, text="Adicionar", command=adicionar_compra)
+adicionar_compra_button = tk.Button(adicionar_compra_frame, text="Adicionar", command=lambda: adicionar_compra(cursor, banco))
 adicionar_compra_button.grid(row=4, column=0, columnspan=2)
 
-# Área de exibição de resultados
-resultado_text = tk.Text(janela, height=10, width=40)
-resultado_text.config(state=tk.DISABLED)
-resultado_text.pack()
-
-
-# Executa a interface gráfica
-janela.mainloop()
+# Execute o loop principal da GUI
+root.mainloop()
 
 # Fechando a conexão com o banco de dados
 banco.close()
